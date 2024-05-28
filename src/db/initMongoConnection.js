8688a744b655be99e574e79939d85d35e6d73dea
Veakor <135655247+Veakor@users.noreply.github.com>
2024-05-28 16:05:39 +0200

    add

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,12 +3,7 @@ import { env } from '../utils/env.js';
 import { ENV_VARS } from '../constants/constants.js';
 
 export const initMongoConnection = async () => {
-  const connectionLink = `mongodb+srv://${env(ENV_VARS.MONGODB_USER)}:${env(
-    ENV_VARS.MONGODB_PASSWORD,
-  )}@${env(ENV_VARS.MONGODB_URL)}/${env(
-    ENV_VARS.MONGODB_DB,
-  )}?retryWrites=true&w=majority&appName=Cluster0`;
-
+  const connectionLink = mongodb+srv://korolevavea:<password>@students.jxprxzq.mongodb.net/?retryWrites=true&w=majority&appName=Students};
   try {
     await mongoose.connect(connectionLink);
     console.log('Mongo connection successfully established!');
