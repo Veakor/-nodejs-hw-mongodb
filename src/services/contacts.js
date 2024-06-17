df67a8c4789fb233f891a7e76d806d3a9968849f
Veakor <135655247+Veakor@users.noreply.github.com>
2024-06-17 10:23:51 +0200

    add

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -12,17 +12,12 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactQuery = Contact.find();
+  const contactQuery = Contact.find(filter);
 
-  if (filter.contactType) {
-    contactQuery.where('contactType').equals(filter.contactType);
-  }
-  if (filter.isFavourite) {
-    contactQuery.where('isFavourite').equals(filter.isFavourite);
-  }
+
 
   const [contactCount, contacts] = await Promise.all([
-    Contact.find().merge(contactQuery).countDocuments(),
+  contactQuery.countDocuments(),
     contactQuery
       .skip(skip)
       .limit(limit)
