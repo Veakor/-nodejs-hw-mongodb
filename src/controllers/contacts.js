c27b0f7c9b9ae513232771c9e1faa82ea6ca7c4e
Veakor <135655247+Veakor@users.noreply.github.com>
2024-06-20 11:32:30 +0200

    add

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -26,7 +26,6 @@ export const getContactsController = async (req, res, next) => {
     message: 'Successfully found contacts!',
     data: {
       ...paginationData,
-      data: contacts,
     },
   });
 } catch (error) {
