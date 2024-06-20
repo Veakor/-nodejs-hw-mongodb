057e08f24a27175150b031a4c3a01e03439bfddb
Veakor <135655247+Veakor@users.noreply.github.com>
2024-06-20 14:14:13 +0200

    add

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/constants.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
