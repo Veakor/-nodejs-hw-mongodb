df00d940ca5aa3a53ee76b25b48b85b2b7fbc92d
Veakor <135655247+Veakor@users.noreply.github.com>
2024-06-28 06:49:23 +0200

    add

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -24,7 +24,7 @@ const setupSession = (res, session) => {
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
-  res.json({
+  res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
     data: { name: user.name, email: user.email },
@@ -73,8 +73,8 @@ export const logoutUserController = async (req, res) => {
 export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
 
-  res.status(200).json({
-    status: 200,
+  res.status(201).json({
+    status: 201,
     message: 'Reset password email has been successfully sent.',
     data: {},
   });
