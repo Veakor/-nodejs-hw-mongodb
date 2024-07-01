b0b87e3bfcb88eaccfe6ed83ea150844540fcbc2
Veakor <135655247+Veakor@users.noreply.github.com>
2024-07-01 20:51:37 +0200

    add

--- src/routers/contactsRouter.js
+++ src/routers/contactsRouter.js
@@ -14,8 +14,7 @@ import { createContactSchema } from '../validation/createContactSchema.js';
 import { updateContactSchema } from '../validation/updateContactSchema.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { isValidContactId } from '../middleware/isValidContactId.js';
-import { ROLES } from '../constants/constants.js';
-import { checkRoles } from '../middleware/checkRoles.js';
+
 
 
 
@@ -28,40 +27,34 @@ contactsRouter.use('./:contactId', isValidContactId('contactId'));
 
 contactsRouter.get(
   '/',
-  checkRoles(ROLES.ADMIN),
   ctrlWrapper(getContactsController),
 );
 
 contactsRouter.get(
   '/:contactId',
-  checkRoles(ROLES.ADMIN, ROLES.USER),
   ctrlWrapper(getContactByIdController),
 );
 
 contactsRouter.post(
   '/',
-  checkRoles(ROLES.ADMIN),
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
 contactsRouter.put(
   '/:contactId',
-  checkRoles(ROLES.ADMIN),
   validateBody(updateContactSchema),
   ctrlWrapper(putContactController),
 );
 
 contactsRouter.patch(
   '/:contactId',
-  checkRoles(ROLES.ADMIN, ROLES.USER),
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
 contactsRouter.delete(
   '/:contactId',
-  checkRoles(ROLES.ADMIN),
   ctrlWrapper(deleteContactByIdController),
 );
 
